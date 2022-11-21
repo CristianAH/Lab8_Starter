940bac0a69fe06805dd6107aa66c25476bf1660a
Cristian Antonio Hernandez <cristianantoniohernandez@Cristians-MacBook-Pro>
2022-11-20 17:19:55 -0800

    Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Lab 8 - Starter
+
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+(1) Within a Github action that runs whenever code is pushed to ensure that any new code meets the standards set by the organization and be bug free before being able to use the code for development/production.
+
+2.  Would you use an end to end test to check if a function is returning the correct output?
+
+Yes
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No since we are not able to take into account external factors that may affect sending the message.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes because we are able to quantify the test message to determine whether it exceeds the max message length.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,42 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//isPhoneNumber
+test('checks (858) 534-2230 is a valid phone number.', ()=> {
+    expect(functions.isPhoneNumber('(858) 534-2230')).toBe(true);
+});
+test('checks empty string is not a valid phone', () => {
+    expect(functions.isPhoneNumber('')).toBe(false);
+});
+
+//isEmail
+test('checks servicedesk@ucsd.edu is a valid email', () =>{
+    expect(functions.isEmail('servicedesk@ucsd.edu')).toBe(true);
+});
+test('checks empty string is not a valid email', () =>{
+    expect(functions.isEmail('')).toBe(false);
+});
+
+//isStrongPassword
+test('checks weBb63M92 is a strong password', () =>{
+    expect(functions.isStrongPassword('weBb63M92')).toBe(true);
+});
+test('checks empty string is not a strong password', () =>{
+    expect(functions.isStrongPassword('')).toBe(false);
+});
+
+//isDate
+test('check 11/20/2022 is a valid date', () =>{
+    expect(functions.isDate('11/20/2022')).toBe(true);
+});
+test('checks nov 20 2022 is not a valid date', () =>{
+    expect(functions.isDate('nov 20 2022')).toBe(false);
+});
+
+//isHexColor
+test('checks EAECEE is a valid character hex code', () =>{
+    expect(functions.isHexColor('EAECEE')).toBe(true);
+});
+test('checks CSE110L is not a valid character hex code', () =>{
+    expect(functions.isHexColor('CSE110L')).toBe(false);
+});
